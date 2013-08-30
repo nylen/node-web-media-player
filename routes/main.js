5a34f0c698f19849b6ec9c36d9bbf879b6b01963
James Nylen <jnylen@gmail.com>
2013-08-30 04:53:04 +0000

    Require authentication

--- routes/main.js
+++ routes/main.js
@@ -6,27 +6,27 @@ var log      = require('../lib/logger'),
 exports.setRoutes = function(app, config, listen) {
     player.setConfig(config.player);
 
-    app.get('/', function(req, res) {
+    app.get('/', app.ensureAuthenticated, function(req, res) {
         res.redirect(config.app.namespace + '/browse');
     });
 
-    app.get('/browse', function(req, res) {
+    app.get('/browse', app.ensureAuthenticated, function(req, res) {
         // res.send(player.getBrowseData(''));
         res.render('index.html', player.getBrowseData(''));
     });
 
-    app.get('/browse/*', function(req, res) {
+    app.get('/browse/*', app.ensureAuthenticated, function(req, res) {
         res.render('index.html', player.getBrowseData(req.params[0]));
     });
 
-    app.get('/play/*', function(req, res) {
+    app.get('/play/*', app.ensureAuthenticated, function(req, res) {
         var filePath = player.validatePath(req.params[0], true);
         player.playFile(filePath, function() {
             res.redirect(302, config.app.namespace + '/browse/' + path.dirname(filePath.rel));
         });
     });
 
-    app.get('/control/:command', function(req, res) {
+    app.get('/control/:command', app.ensureAuthenticated, function(req, res) {
         player.sendCommand(req.params.command, function() {
             res.redirect(req.header('Referer'));
         }, req.query);
