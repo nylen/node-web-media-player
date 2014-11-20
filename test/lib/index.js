6f1c6f78e25497ac82e6d011126fb4ded514489b
James Nylen <jnylen@gmail.com>
2014-11-20 14:08:04 -0600

    Behave better if a test server is already started elsewhere
    - Don't try to start another server
    - Don't drop the database when closing the server (this does make the
      base template tests fail, so need to exclude them)

--- test/lib/index.js
+++ test/lib/index.js
@@ -38,49 +38,79 @@ exports.ensureConnected = function(done) {
     }
 };
 
-exports.startServer = function(done) {
-    exports.server = cp.spawn('node', ['server.js'],
-        (process.env.DEBUG_SERVER ? { stdio : 'inherit' } : null));
+function checkPort(port, cb, timeout) {
+    timeout = timeout || 5000;
 
-    function checkPort(port, cb, timeout) {
-        timeout = timeout || 5000;
+    var start    = +new Date,
+        interval = 500;
 
-        var start    = +new Date,
-            interval = 500;
-
-        portscanner.checkPortStatus(port, 'localhost', function(err, status) {
-            if (err) {
-                cb(err);
-            } else if (status == 'open') {
-                cb(null);
+    portscanner.checkPortStatus(port, 'localhost', function(err, status) {
+        if (err) {
+            cb(err);
+        } else if (status == 'open') {
+            cb(null);
+        } else {
+            timeout -= new Date - start + interval;
+            if (timeout < 0) {
+                cb(new Error('Port not open yet'));
             } else {
-                timeout -= new Date - start + interval;
-                if (timeout < 0) {
-                    cb(new Error('Port not open yet'));
-                } else {
-                    setTimeout(function() {
-                        checkPort(port, cb, timeout);
-                    }, interval);
-                }
+                setTimeout(function() {
+                    checkPort(port, cb, timeout);
+                }, interval);
             }
-        });
-    }
+        }
+    });
+}
+
+exports.startServer = function(done) {
+    // If the port is already open, that's probably because we're running a
+    // test server somewhere else, which is fine.
+    //
+    // Use this command to start a test server manually:
+    //
+    //   NODE_CONFIG_DIR=test/config/ node server.js
 
     checkPort(config.app.port, function(err) {
-        if (err) throw err;
-        // Give the server a little extra time to load, I got ECONNRESET
-        // once
-        setTimeout(function() {
-            exports.dropDatabase(done);
-        }, 300);
-    });
+        if (err) {
+            // The port is not open - start a server.
+
+            exports.server = cp.spawn('node', ['server.js'],
+                (process.env.DEBUG_SERVER ? { stdio : 'inherit' } : null));
+
+            checkPort(config.app.port, function(err) {
+                if (err) throw err;
+                // Give the server a little extra time to load, I got ECONNRESET
+                // once
+                setTimeout(function() {
+                    exports.dropDatabase(done);
+                }, 300);
+            });
+
+        } else {
+            // The port is open.  Since the test server isn't ours, don't drop
+            // the database to avoid killing sessions in between test runs.
+
+            console.error(
+                'Port %d is already open, not starting a server',
+                config.app.port);
+
+            done();
+
+        }
+    }, 0);
+
 };
 
 exports.closeServer = function(done) {
-    exports.server.once('exit', function() {
-        exports.dropDatabase(done);
-    });
-    exports.server.kill('SIGINT');
+    if (exports.server) {
+        exports.server.once('exit', function() {
+            exports.dropDatabase(done);
+            exports.server = null;
+        });
+        exports.server.kill('SIGINT');
+    } else {
+        done();
+    }
 };
 
 exports.createBrowser = function() {
